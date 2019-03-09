c835a091bb0505dcf3ff156a69af7256dc37fb72
parkjongsoo <newtein80@gmail.com>
2019-03-10 01:05:59 +0900

    bks spring boot 03 test ing..02

--- src/main/java/com/springboot/bksboot2/Application.java
+++ src/main/java/com/springboot/bksboot2/Application.java
@@ -1,15 +1,19 @@
 package com.springboot.bksboot2;
-// 현재 위치가 root pack
+// 현재 위치가 root package
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication // 스프링 부트 가이드에서처럼 만들 수 없어서 주석 처리했더니 에러 발생하여 주석 삭제하였다.
+//@SpringBootApplication // 스프링 부트 가이드에서처럼 만들 수 없어서 주석 처리했더니 에러 발생하여 주석 삭제하였다.
 @RestController // stereotype annotation. In this case, this controller handling incoming web request
 @EnableAutoConfiguration // 의존성과 관련하여 자동 설정 파일이 적용됨. spring starter web 이 설정되어있을때 특정 설정파일이 적용되도록 되어있음
+@Configuration
+@ComponentScan
 public class Application {
 
 	public static void main(String[] args) {
