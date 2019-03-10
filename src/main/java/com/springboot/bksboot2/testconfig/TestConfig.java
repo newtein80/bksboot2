a6b3ca1a9610f6397265934a4e3923bc04c3fcea
parkjongsoo <newtein80@gmail.com>
2019-03-10 09:52:35 +0900

    bks spring boot 04 test ing...03

--- src/main/java/com/springboot/bksboot2/testconfig/TestConfig.java
+++ src/main/java/com/springboot/bksboot2/testconfig/TestConfig.java
@@ -1,5 +1,7 @@
 package com.springboot.bksboot2.testconfig;
 
+// import org.springframework.beans.factory.annotation.Autowired;
+
 // import org.springframework.stereotype.Service;
 
 import org.springframework.stereotype.Service;
@@ -11,5 +13,19 @@ import org.springframework.stereotype.Service;
 @Service // @servcie annotation을 삭제 + xml 설정파일 사용 = 테스트 에러 <-- bean으로 등록이 되지 않기때문에
 public class TestConfig {
 
-    
+    // @Autowired
+    // TestConfigRepository testConfigRepository;
+    private final TestConfigRepository testConfigRepository; // 레퍼런스가 바뀌지 않도록 final 을 명시
+
+    // @Autowired
+    public TestConfig(TestConfigRepository testConfigRepository){
+        this.testConfigRepository = testConfigRepository;
+    }
+
+    /**
+     * @return the testConfigRepository
+     */
+    public TestConfigRepository getTestConfigRepository() {
+        return testConfigRepository;
+    }
 }
---
+++ src/main/java/com/springboot/bksboot2/testconfig/TestConfigRepository.java
@@ -0,0 +1,12 @@
+package com.springboot.bksboot2.testconfig;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * TestConfigRepository
+ */
+@Repository
+public class TestConfigRepository {
+
+    
+}
